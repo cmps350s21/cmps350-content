7e7b2c0db92165357dc1102b6974d27f87ad786c
Abdelkarim Erradi <erradi@live.com>
2021-02-14 07:53:33 +0300

    JavaScript-OOP slides

---
+++ examples/04-5.JavaScript/3.operators-expressions/7.es7-exponential-operator.js
@@ -0,0 +1,9 @@
+// exponential operator (**)
+const x = 3
+let y = 2
+console.log(x, y);
+x = 8;
+let result = Math.pow(x,y);
+// … produces the same result as …
+result = x ** y;
+console.log(result);
