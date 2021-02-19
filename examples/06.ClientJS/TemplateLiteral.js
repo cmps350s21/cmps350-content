53c5fce82085d85bfe4fae5a9973ca9c17cec7a6
Abdelkarim Erradi <erradi@live.com>
2021-02-19 23:51:17 +0300

    Update TemplateLiteral.js

--- examples/06.ClientJS/TemplateLiteral.js
+++ examples/06.ClientJS/TemplateLiteral.js
@@ -21,19 +21,38 @@ const person = {
     hobbies: ['Make people laugh', 'Do silly things', 'Visit interesting places']
 }
 
-function personTemplate({name, hobbies, job}){
-    return `<article class="person">
-               <h3>${name}</h3>
-               <p>Current job: ${job}</p>
-               <div>
-                   <div>Hobbies:</div>
-                   <ul>
-                       ${hobbies.map(hobby => `<li>${hobby}</li>`).join(" ")}
-                   </ul>
-               </div>
+const personTemplate = ({name, hobbies, job}) =>
+    `<article class="person">
+       <h3>${name}</h3>
+       <p>Current job: ${job}</p>
+       <div>
+           <div>Hobbies:</div>
+           <ul>
+               ${hobbies.map(hobby => `<li>${hobby}</li>`).join(" ")}
+           </ul>
+       </div>
     </article>`;
-}
 
 const personHtml = personTemplate(person);
 // document.body.innerHTML = personHtml; // to display it on the browser
 console.log(personHtml);
+
+const payment = {
+    date: '1/2/2021',
+    name: 'Mr Bean',
+    amount: 200,
+    reason: 'Donation',
+    receiver: 'Juha'
+}
+
+const receiptTemplate = (payment) =>
+    `<div>
+      <p>Date: ${payment.date}</p>
+      <p>Received from: ${payment.name}, the amount of QR ${payment.amount}</p>
+      <p>For: ${payment.reason}</p>
+      <p>Received by: ${payment.receiver}</p>
+    </div>`
+
+console.log(receiptTemplate(payment));
+// Render the template in the DOM
+document.body.innerHTML = receiptTemplate(payment);
